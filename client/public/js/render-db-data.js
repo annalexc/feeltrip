78d16552c6da61a57cae517c4d98ec3c6475ff02
Matthew Jelling <Jelling@MacBook-Pro.local>
2016-05-12 10:57:00 -0400

    mid morning commit

--- client/public/js/render-db-data.js
+++ client/public/js/render-db-data.js
@@ -1,24 +1,37 @@
-var renderSnapshots = function(snapShot){
+console.log("Yo");
+
+
+var renderSnapshots = function(snapshotData){
+  console.log("Snapshot Data in Render Snapshots is", snapshotData);
+  // var $location = $('#place').text();
+  // var $tweetsSaved = $('#tweets-saved');
+  // $tweetsSaved.empty();
+  // for(var i = 0; i<snapshotData.length; i++){
+  //   if($location == snapshotData[i].location){
+  //     var tweet = snapshotData[i].tweet_data;
+  //     for(var x = 0; x< snapshotData[i].tweet_data.length; x++){
+  //       var appendTweetText = ($('<p>').class('tweet-text').text(tweet[x].text));
+  //       var appendTweetInfo = ($('<p>').class('info'));
+  //       var appendScreenName = ($('<span>').class('screen-name').text(tweet[x].screen_name));
+  //       var appendPostedAt = ($('<span>').class('posted-at').text(tweet[x].posted_at));
+  //       $tweetsSaved.append($('<div>').class('tweet').append(appendTweetText));
+  //     };
+  //   };
+  // };
+};
+
+function renderSaved(){
   var userID = Cookies.getJSON("current_user")._id;
-  var $location = $('#place').text();
-  var $tweetsSaved = $('#tweets-saved')
-  for(var i = 0; i<snapshotData.length; i++){
-    if($location == snapshotData[i].location){
-      var tweet = snapshotData[i].tweet_data;
-      for(var x = 0; x< snapshotData[i].tweet_data.length; x++){
-      var appendTweetText = ($('<p>').class('tweet-text').text(tweet[x].text);
-      var appendTweetInfo = ($('<p>').class('info')
-      var appendScreenName = ('-'$('<span>').class('screen-name').text(tweet[x].screen_name)', ');
-      var appendPostedAt = ('-'$('<span>').class('posted-at').text(tweet[x].posted_at);
-      $tweetsSaved.append($('<div>').class('tweet').append(appendTweetText, appendTweetInfo, appendScreenName, appendPostedAt);
+  $.ajax({
+    url: "/api/snapshots/"+userID,
+    method: "get",
+    success: function(snapshotData){
+    //data.forEach( renderSnapshots );
+    // renderSnapshots(snapShotData);
+    console.log("Snapshot Data is:", snapshotData);
+    renderSnapshots(snapshotData);
     }
-    var
-  }
+  });
+};
 
-}
-
-$.ajax({ url: "/api/snapshots",
-  method: "get"
-}).success(function(snapshotData){
-  data.forEach( renderSnapshots );
-});
+//renderSaved();
